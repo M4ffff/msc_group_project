f5c15fa0f774514444996c8ca02db5c288931961
benyiyun <benyiyun.mitchell2@gmail.com>
2025-01-30 00:50:29 +0000

    Grammar corrections
    Small grammar corrections to supervised learning section

--- pages/1 Supervised learning.py
+++ pages/1 Supervised learning.py
@@ -6,12 +6,12 @@ import time
 st.title("Supervised Learning ")
 st.write("🎉🎉🎉If you've made it this far, it means you're genuinely interested in this topic, and the content ahead is definitely worth looking forward to!🎉🎉🎉")
 
-st.header("1.What is Supervised Learning ")
-st.write("&nbsp;&nbsp;&nbsp;&nbsp;Supervised learning,also known as supervised learning or supervised learning,is a method in machine learning that can learn or establish a pattern (function/learning model) from training data and use this pattern to infer new instances.The training data consists of input objects (usually vectors) and expected outputs. The output of the function can be a continuous value (referred to as regression analysis) or the prediction of a classification label (known as classification).")
+st.header("1. What is Supervised Learning ")
+st.write("&nbsp;&nbsp;&nbsp;&nbsp;Supervised learning, also known as supervised learning or supervised learning, is a method in machine learning that can learn or establish a pattern (function/learning model) from training data and use this pattern to infer new instances. The training data consists of input objects (usually vectors) and expected outputs. The output of the function can be a continuous value (referred to as regression analysis) or the prediction of a classification label (known as classification).")
 st.write("&nbsp;&nbsp;&nbsp;&nbsp;The task of a supervised learner is to predict the output of this function for any possible input after observing some pre-labeled training examples (inputs and expected outputs). To achieve this, the learner must generalize from the existing data to unobserved cases in a reasonable manner (see inductive bias). In human and animal perception, this is commonly referred to as concept learning.")
 st.write("Description from [Wikipedia](https://en.wikipedia.org/wiki/Supervised_learning)")
 
-st.header("2.What is Supervised Learning ")
+st.header("2. What is Supervised Learning ")
 st.write("&nbsp;&nbsp;&nbsp;&nbsp;Supervised learning is one of the most common methods in machine learning. It trains models using labeled training data, enabling the models to make predictions or classifications on new, unseen data. ")
 st.write("&nbsp;&nbsp;&nbsp;&nbsp;Due to its powerful predictive capabilities and broad applicability, supervised learning has a wide range of applications in many fields. Here are some of the main application areas:")
 st.subheader("&nbsp;&nbsp;1. Image Recognition")
@@ -38,7 +38,7 @@ col1, col2, col3 = st.columns([1, 2, 3])
 with col2:
  st.image("images/OIP.jpg", caption="OIP", width=300)
 
-st.write("&nbsp;&nbsp;&nbsp;&nbsp;Of course, there are many other application scenarios.Supervised learning is widely applied and deeply integrated into various aspects of daily life, making it an accessible and practical tool rather than something distant or unattainable.")
+st.write("&nbsp;&nbsp;&nbsp;&nbsp;Of course, there are many other application scenarios. Supervised learning is widely applied and deeply integrated into various aspects of daily life, making it an accessible and practical tool rather than something distant or unattainable.")
 
 st.header("3.What kind of Supervised Learning do you like")
 st.write("&nbsp;&nbsp;&nbsp;&nbsp;But no matter where it is applied, the underlying logic is consistent. Starting from the basics is a necessary path to becoming a master. Choose a direction that interests you and dive in.")
