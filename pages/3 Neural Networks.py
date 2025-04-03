66f19e5dd1a192c39513eb42a563b0a58d56910f
Elliot Ayliffe <Elliot.ayliffe@gmail.com>
2025-04-03 17:40:27 +0100

    Update 3 Neural Networks.py

--- pages/3 Neural Networks.py
+++ pages/3 Neural Networks.py
@@ -556,7 +556,15 @@ with sections[2]:
             st.markdown("**Model Summary:**")
             io_file = io.StringIO()
             model.summary(print_fn=lambda x: io_file.write(x + '\n'))
-            st.text(io_file.getvalue())
+
+            summary_text = io_file.getvalue()
+
+            # If you want to add some styling or spacing, use Markdown formatting
+            formatted_summary = f"<pre>{summary_text}</pre>"
+
+            # Display formatted summary using st.markdown
+            st.markdown(formatted_summary, unsafe_allow_html=True)
+            #st.text(io_file.getvalue())
 
             # Split into training and testing data and call the training function
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
