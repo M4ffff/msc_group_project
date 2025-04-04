57d7742776d2e91c8878f75a63aadef0d1034e13
Matthew Parker <matthewparker0105@yahoo.com>
2025-04-04 11:31:24 +0100

    slight changes to supervised model

--- pages/1 Supervised learning.py
+++ pages/1 Supervised learning.py
@@ -166,7 +166,7 @@ with tab3:
     
         X_train, X_test, y_train, y_test = train_test_split(X[selected_features], y, test_size=0.2, random_state=42)
         
-        model = RandomForestClassifier(n_estimators=100, random_state=42)
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split, random_state=42)
         model.fit(X_train, y_train)
 
         st.write("Feature Importances:", model.feature_importances_)
