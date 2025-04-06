5830c40f4d13a293a0762e3e712415f08000f6a2
Matthew Parker <matthewparker0105@yahoo.com>
2025-04-06 19:28:03 +0100

    initial rf regressor test

--- pages/1 Supervised learning.py
+++ pages/1 Supervised learning.py
@@ -5,6 +5,7 @@ import time
 import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
@@ -274,12 +275,8 @@ with tab4:
     @st.cache_data
     def load_imdb_data():
         df = pd.read_csv('datasets/IMDb_Dataset.csv')
-        
-        # Only keep selected certificates
-        # valid_certificates = ['G', 'PG-13', 'R']
-        # df = df[df['Certificates'].isin(valid_certificates)]
 
-        # Encode 'Certificates'
+        # Encode 'Certificates' and 'Genre'
         encoder = LabelEncoder()
         df['Certificates'] = encoder.fit_transform(df['Certificates'])
         df['Genre'] = encoder.fit_transform(df['Genre'])
@@ -317,7 +314,6 @@ with tab4:
     st.write("Choose which features to include in the model. This helps us explore their influence on the final rating.")
     selected_features_imdb = st.multiselect("Choose features", X_imdb.columns.tolist(), default=X_imdb.columns.tolist())
 
-    st.write(len(X_imdb))
 
     if selected_features_imdb:
         X_train, X_test, y_train, y_test = train_test_split(X_imdb[selected_features_imdb], y_imdb, test_size=0.2, random_state=42)
